ba0fe53cbf70dc205a05715375d73ac2378982b9
Gabriela Borba <gborbas.gb@gmail.com>
2024-09-03 09:09:35 -0300

    Initial commit

---
+++ src/main/java/com/testando/testecardapio/TesteCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.testando.testecardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCardapioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteCardapioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testando/testecardapio/TesteCardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testando.testecardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCardapioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
